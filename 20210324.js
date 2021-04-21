f0d2f45f5c29a610bce8b97787d0f70523af3b4c
hch66 <735582174.com>
2021-04-20 23:10:17 -0700

    0421

--- 20210324.js
+++ 20210324.js
@@ -12,7 +12,7 @@ const server =http.createServer((req,res)=> {
         res.statusCode=200;
         res.setHeader('Content-Type','text/html');
         // var fsData = fs.readFileSync("./test1.html");
-        fs.readFile("./form1.html",(err,fsData)=>{
+        fs.readFile("./form3.html",(err,fsData)=>{
             if(err){
                 console.log("read file");
                 throw err;
@@ -43,26 +43,48 @@ const server =http.createServer((req,res)=> {
         // res.end("end");
     }
     else if(req.url.slice(0,6)=="/input"){
-        res.statusCode=200;
         let url1=req.url.split("?");
         // let urlquery=url1[1].split("&");
         // let firstquery = urlquery[0].split("=");
         // let secondquery = urlquery[1].split("=");
         // let queryData=url1.parse(req,url,true).query;
         let obQuery = querystring.parse(url1[1]);
+        if(obQuery.submit1=="Save"){
+            console.log("ss")
+            fs.writeFile('./savefile',obQuery.name123,(err) =>{
+                if(err) console.log("err");
+                else console.log("writesuccess");
+            })
+        }
+        else{
+            fs.appendFile('./savefile',obQuery.name123,(err) =>{
+                if(err) console.log("err");
+                else console.log("append success");
+            })
+        }
+        res.statusCode=200;
         res.setHeader('Content-Type','text/html');
-        res.write(obQuery.name+"<br>");
-        res.write(obQuery.submit1+"<br>");
-        res.end("submit OK");
+        fs.readFile("./form3.html",(err,fsData)=>{
+            if(err){
+                console.log("read file");
+                throw err;
+            }
+            res.write(fsData);
+            res.end();
+        })
+
+        // res.setHeader('Content-Type','text/html');
+        // res.write(obQuery.name123+"<br>");
+        // res.write(obQuery.submit1+"<br>");
+        // res.end("submit OK");
     }
     else{
         res.statusCode=200;
         res.setHeader('Content-Type','text/html');
-        // res.write(req.url)
+
         res.write('<h1> hch666,you are the '+i+'th vistor');
         res.end();
     }
-    // res.write('<h1> hch666,you are the '+i+'th vistor');
     
     i++;
 
